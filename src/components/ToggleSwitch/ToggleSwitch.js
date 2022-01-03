0025b3568b550532ecb197f0de3755b15dfb19ee
Keith O'Rourke <ryan_orourke@live.com>
2022-01-03 14:02:35 -0500

    Added daymode and nightmode icons to toggleswitch

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,10 +1,17 @@
 import "./ToggleSwitch.scss"
+import sun from "../../assets/icons/sun-line.svg"
+import moon from "../../assets/icons/moon-line.svg"
+
 
 export default function ToggleSwitch({handleCheck}) {
     return (
-        <label htmlFor="checkbox" className="darkmode">
-            <input type="checkbox" onChange={handleCheck} className="darkmode__button off" id="checkbox" />
-            <span className="darkmode__slider"></span>
-        </label>
+        <div class="container">
+            <img class="icon" src={sun} alt="Sun"/>
+            <label htmlFor="checkbox" className="darkmode">
+                <input type="checkbox" onChange={handleCheck} className="darkmode__button off" id="checkbox" />
+                <span className="darkmode__slider"></span>
+            </label>
+            <img class="icon" src={moon} alt="Moon" />
+        </div>
     )
 }
